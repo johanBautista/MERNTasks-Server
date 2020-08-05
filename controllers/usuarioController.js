a8f26e9d63d05d26856d664e73ef56950c92a6a3
johan Bautista <jsbparra@gmail.com>
2020-08-05 23:27:35 +0200

    validacion usuario y hashear password con bcrypt

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,15 +1,28 @@
 const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
 
 exports.crearUsuario = async (req, res) => {
+  // destructuring email, pass
+  const { email, password } = req.body;
+
   try {
-    let usuario;
+    //verificar si hay usuario igual
+    let usuario = await Usuario.findOne({ email });
+    if (usuario) {
+      return res.status(400).json({ msg: 'El usuario ya existe' });
+    }
 
     //crea nuevo usuario
     usuario = new Usuario(req.body);
+
+    //hashear password
+    const salt = await bcryptjs.genSalt(10);
+    usuario.password = await bcryptjs.hashSync(password, salt);
+
     // guardar nuevo usuario
     await usuario.save();
     //mensaje de confirmacion
-    res.send('Usuario creado correctamente');
+    res.json({ msg: 'Usuario creado correctamente' });
   } catch (error) {
     console.log(error);
     res.status(400).send('Error al crear Usuario');
