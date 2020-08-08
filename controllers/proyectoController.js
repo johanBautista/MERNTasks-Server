29aeb88758ec50775f1f60de02325c1e8203adaa
johan Bautista <jsbparra@gmail.com>
2020-08-08 17:37:47 +0200

    obtener proyectos usuario.id

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -2,12 +2,12 @@ const Proyecto = require('../models/Proyecto');
 const { validationResult } = require('express-validator');
 
 exports.crearProyecto = async (req, res) => {
-    //revisar si hay errores
-    const errores = validationResult(req);
-    if (!errores.isEmpty()) {
-      return res.status(400).json({ errores: errores.array() });
-    }
-    
+  //revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
   try {
     // crear nuevo proyecto
     const proyecto = new Proyecto(req.body);
@@ -23,7 +23,20 @@ exports.crearProyecto = async (req, res) => {
   }
 };
 
-exports.obtenerProyecto = async (req, res) => {};
+//obtener proyectos del usuario actual
+exports.obtenerProyecto = async (req, res) => {
+  try {
+    const proyectos = await Proyecto.find({ creador: req.usuario.id }).sort({
+      creado: -1,
+    });
+    res.json({ proyectos });
+    console.log('uno:', req.usuario);
+    console.log('dos:', req.body);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Hubo un error');
+  }
+};
 
 exports.actualizarProyecto = async (req, res) => {};
 
